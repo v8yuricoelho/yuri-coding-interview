4b6b6a55c8b14f9b4cbbe0243794f8d5e1a4efd7
Yuri <ycr.data@gmail.com>
2025-04-11 14:41:31 -0300

    using regex to optimize username filter

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   belongs_to :company
 
   scope :by_company, -> (identifier) { where(company: identifier) if identifier.present? }
-  scope :by_username, -> (username) { where('username LIKE ?', username) if username.present? }
+  scope :by_username, -> (username) { where('username LIKE ?', "%#{username}") if username.present? }
 end
